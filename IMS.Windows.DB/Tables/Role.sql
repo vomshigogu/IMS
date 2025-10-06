31c9d88f078e74503ae307f6cea131318cca7e4e
vamshigogu <vamshi.gogu@yahoo.com>
2025-10-06 09:51:43 +0530

    adding first role table to db project

---
+++ IMS.Windows.DB/Tables/Role.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Role]
+(
+	[Id]             INT             NOT NULL    PRIMARY KEY Identity(1,1),
+	[Name]			 VARCHAR(50)         NULL,
+	[Code]           VARCHAR(50)         NULL,
+	[CreatedBy]      INT                 NULL,
+	[CreatedOn]      DATETIME            NULL,
+	[ModifiedBy]     INT                 NULL,
+	[ModifiedOn]     DATETIME            NULL,
+	[IsActive]       BIT                 NULL
+)
