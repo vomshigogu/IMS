df47735fe7471cb0f8380ff0d7ff59224d48a8bb
vamshigogu <vamshi.gogu@yahoo.com>
2025-10-11 10:56:40 +0530

    Adding Categories table

---
+++ IMS.Windows.DB/Tables/Categories.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Categories]
+(
+	 
+    CategoryID INT PRIMARY KEY IDENTITY(1,1),
+    CategoryName NVARCHAR(100) NOT NULL,
+    Description NVARCHAR(500),
+    ParentCategoryID INT NULL,
+    IsActive BIT DEFAULT 1,
+    CreatedDate DATETIME DEFAULT GETDATE(),
+    ModifiedDate DATETIME DEFAULT GETDATE(),
+    FOREIGN KEY (ParentCategoryID) REFERENCES Categories(CategoryID)
+);
+
